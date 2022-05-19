a5e9babed5ea47f8ac1e5f16ae4c646f1d5f8f24
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2022-05-18 23:50:00 -0400

    Initial creation of the repository

---
+++ fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+
+game 'gta5'
+lua54 'yes'
+
+name 'JLRP-Menu'
+author 'Mahan Moulaei'
+discord 'Mahan#8183'
+description 'JolbakLifeRP Menu'
+
+version '0.0'
+
+shared_scripts {
+    '@JLRP-Framework/imports.lua',
+	'@JLRP-Framework/shared/locale.lua',
+	'locales/*.lua',
+	'config.lua'
+}
+
+client_scripts {
+	'client/default_menu.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	'html/ui.html',
+	'html/css/*.css',
+	'html/javascript/*.js'
+}
+
+dependencies {
+	'JLRP-Framework'
+}
